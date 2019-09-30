06e622586a38093097171bf7d7588ac452c9fab1
Bartoshko <lenart.consulting@gmail.com>
2019-09-30 15:41:08 +0200

    fix indent

--- _ts/canvas/grid-matrix.ts
+++ _ts/canvas/grid-matrix.ts
@@ -1,34 +1,35 @@
 export class GridMatrix {
-  grid: number[][];
+    grid: number[][];
 
-  constructor() {
+    constructor() {
     this.grid = [];
-  }
+    }
 
-  isSmallerThanNextGradient_X(x: number, y: number): boolean {
+    isSmallerThanNextGradient_X(x: number, y: number): boolean {
     return this.grid[x + 1] && this.grid[x + 1][y] < this.grid[x][y];
-  }
+    }
 
-  isSmallerThanBeforeGradient_X(x: number, y: number): boolean {
-   return this.grid[x - 1] && this.grid[x - 1][y] < this.grid[x][y];
-  }
+    isSmallerThanBeforeGradient_X(x: number, y: number): boolean {
+    return this.grid[x - 1] && this.grid[x - 1][y] < this.grid[x][y];
+    }
 
-  isSmallerThanNextGradient_Y(x: number, y: number): boolean {
+    isSmallerThanNextGradient_Y(x: number, y: number): boolean {
     return this.grid[x][y + 1] < this.grid[x][y];
-  }
-  isSmallerThanBeforeGradient_Y(x: number, y: number): boolean {
+    }
+    
+    isSmallerThanBeforeGradient_Y(x: number, y: number): boolean {
     return this.grid[x][y - 1] < this.grid[x][y];
-  }
+    }
 
-  isAboveAverage(average: number, x: number, y: number): boolean {
+    isAboveAverage(average: number, x: number, y: number): boolean {
     return this.grid[x][y] > average
-  }
+    }
 
-  hasLength(): boolean {
+    hasLength(): boolean {
     return this.grid.length > 0;
-  }
+    }
 
-  pickRandomIndexToAvoidBias(): number {
+    pickRandomIndexToAvoidBias(): number {
     return Math.floor(Math.random() * this.grid.length);
-  }
+    }
 }
